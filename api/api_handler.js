57eff1e6092f3ae2e1ed56214ac42939de51a23e
Shruti <katdareshruti@yahoo.com>
2020-04-24 22:44:04 -0700

    Assignment 6

---
+++ api/api_handler.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const product = require('./product.js');
+
+const resolvers = {
+    Query: {
+      productList: product.list,
+      product: product.get,
+    },
+    Mutation: {
+      productAdd: product.add,
+      productUpdate: product.update,
+      productDelete: product.delete,
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+    resolvers,
+    formatError: (error) => {
+      console.log(error);
+      return error;
+    },
+  });
+
+  function installHandler(app) {
+
+    server.applyMiddleware({app, path: '/graphql'});
+  }
+
+  module.exports = { installHandler };
---
+++ ui/src/ProductReport.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductAddItem() {
+    return (
+        <div>
+            <h2>This is a placeholder for the product add item</h2>
+        </div>
+    );
+}
